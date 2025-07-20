b09a30ef055e83ad333c3f1716d682ca2108578a
Yue <liyue3780@gmail.com>
2025-07-20 19:44:01 -0400

    add 150 epochs

--- nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py
+++ nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py
@@ -48,6 +48,13 @@ class nnUNetTrainer_100epochs(nnUNetTrainer):
         self.num_epochs = 100
 
 
+class nnUNetTrainer_150epochs(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 150
+
+
 class nnUNetTrainer_250epochs(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
                  device: torch.device = torch.device('cuda')):
